8a8d0c8a6506720c99080e5c24b46232c27d5dbe
josh-deprogram <josh@themerrier.com>
2018-10-04 16:21:27 +1000

    scroll check fix

--- Libraries/Lists/VirtualizedList.js
+++ Libraries/Lists/VirtualizedList.js
@@ -366,7 +366,7 @@ class VirtualizedList extends React.PureComponent<Props, State> {
     /* $FlowFixMe(>=0.53.0 site=react_native_fb,react_native_oss) This comment
      * suppresses an error when upgrading Flow's support for React. To see the
      * error delete this comment and run Flow. */
-    this._scrollRef.scrollTo(
+    this._scrollRef.scrollTo && this._scrollRef.scrollTo(
       /* $FlowFixMe(>=0.53.0 site=react_native_fb,react_native_oss) This
        * comment suppresses an error when upgrading Flow's support for React.
        * To see the error delete this comment and run Flow. */
@@ -388,7 +388,7 @@ class VirtualizedList extends React.PureComponent<Props, State> {
     /* $FlowFixMe(>=0.53.0 site=react_native_fb,react_native_oss) This comment
      * suppresses an error when upgrading Flow's support for React. To see the
      * error delete this comment and run Flow. */
-    this._scrollRef.flashScrollIndicators();
+    this._scrollRef.flashScrollIndicators && this._scrollRef.flashScrollIndicators();
   }
 
   /**
